0849c93b6382b4bb4b6aea588c78a713cf357f12
Naishu Kui <40501750+naishukui@users.noreply.github.com>
2025-05-06 14:51:25 -0500

    Add files via upload

--- R/bias_linear_nleqslv.R
+++ R/bias_linear_nleqslv.R
@@ -27,19 +27,8 @@
 #' )
 #' result <- bias_linear_nleqslv(beta_list, cov_mat_list, mu_list)
 #' print(result)
-beta_list <- list(beta_0 = 1, beta_X = c(0.7, -0.3), beta_G = c(0.2, 0.4))
-mu_list <- list(mu_X = c(4, 2))
-cov_mat_list <- list(
-    mu_XX = matrix(c(5, 3, 3, 4), nrow = 2),
-    mu_X_tildeG = matrix(c(0.1, 0.2, 0.3, 0.6), nrow = 2),
-    mu_XG = matrix(c(0.5, 0.6, 0.7, 0.8), nrow = 2),
-    mu_tildeG_tildeG = matrix(c(1, 0.5, 0.5, 1), nrow = 2),
-   mu_tildeG_G = matrix(c(0.9, 0.8, 0.7, 0.6), nrow = 2)
-  )
-bias_nleqslv(beta_list, cov_mat_list, mu_list)
-bias_linear(beta_list, cov_mat_list, mu_list)
 
-bias_nleqslv <- function(beta_list, cov_mat_list, mu_list) {
+bias_linear_nleqslv <- function(beta_list, cov_mat_list, mu_list) {
   # Load required package
   if (!requireNamespace("nleqslv", quietly = TRUE)) {
     stop("Package 'nleqslv' required but not installed.")
@@ -102,37 +91,4 @@ bias_nleqslv <- function(beta_list, cov_mat_list, mu_list) {
   )
 
   return(result)
-}
-bias_linear <- function(beta_list, cov_mat_list, mu_list) {
-  beta_0 <- beta_list$beta_0
-  beta_X <- as.matrix(beta_list$beta_X)
-  beta_G <- as.matrix(beta_list$beta_G)
-
-  mu_X <- as.matrix(mu_list$mu_X)
-  mu_XX <- cov_mat_list$mu_XX
-  mu_X_tildeG <- cov_mat_list$mu_X_tildeG
-  mu_XG <- cov_mat_list$mu_XG
-  mu_tildeG_tildeG <- cov_mat_list$mu_tildeG_tildeG
-  mu_tildeG_G <- cov_mat_list$mu_tildeG_G
-
-  mu_tildeG_X <- t(mu_X_tildeG)
-
-  # Compute O = μ_XX^T - μ_X μ_X^T and its inverse
-  O <- mu_XX - (mu_X %*% t(mu_X))
-  solve_O <- solve(O)
-
-  # Compute Q and R for α_G
-  Q <- mu_tildeG_tildeG - mu_tildeG_X %*% solve_O %*% mu_X_tildeG
-  R <- (mu_tildeG_G -  mu_tildeG_X %*% solve_O %*% mu_XG) %*% beta_G
-  alpha_G <- solve(Q, R)  # Solves Q α_G = R
-
-  # Compute P and α_x
-  P <- (mu_X_tildeG %*% alpha_G) - (mu_XG %*% beta_G)
-  alpha_X <- beta_X - solve_O %*% P
-
-
-  # Compute α_0
-  alpha_0 <- beta_0 + (t(mu_X) %*% beta_X) - (t(mu_X) %*% alpha_X)
-
-  return(list(alpha_0 = alpha_0, alpha_X = alpha_X, alpha_G = alpha_G))
 }
